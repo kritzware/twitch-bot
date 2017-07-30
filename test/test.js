b4fc01818f064aac2fa46014121af6808fdc3d00
kritzware <suspiciouspotato@gmail.com>
2017-07-30 19:52:27 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,47 @@
+const assert = require('assert')
+const expect = require('chai').expect
+
+const TwitchBot = require('../index')
+
+
+
+describe('TwitchBot()', () => {
+  it('should create a new Bot instance', () => {
+    const bot = new TwitchBot({
+      username: CONFIG.USERNAME,
+      oauth: CONFIG.OAUTH,
+      channel: CONFIG.CHANNEL
+    })
+    expect(bot).to.be.an.instanceOf(TwitchBot)
+  })
+  it('should throw an error when missing required arguments', () => {
+    try {
+      const bot = new TwitchBot({})
+    } catch(err) {
+      expect(err.message).to.equal('missing required arguments')
+    }
+  })
+  it('should normalize the channel name', () => {
+    const bot = new TwitchBot({
+      username: CONFIG.USERNAME,
+      oauth: CONFIG.OAUTH,
+      channel: 'Channel'
+    })
+    const bot2 = new TwitchBot({
+      username: CONFIG.USERNAME,
+      oauth: CONFIG.OAUTH,
+      channel: '#ChanneL'
+    })
+    expect(bot.channel).to.equal('#channel')
+    expect(bot2.channel).to.equal('#channel')
+  })
+  it('should emit a join event when connecting to twitch irc', done => {
+    const bot = new TwitchBot({
+      username: CONFIG.USERNAME,
+      oauth: CONFIG.OAUTH,
+      channel: CONFIG.CHANNEL
+    })
+    bot.on('join', () => done())
+  })
+
+})
