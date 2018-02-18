1b1015a1184af59afcaf73b94182fa9aff71571e
unknown <anxiety.pb@gmail.com>
2018-02-18 20:13:05 +0100

    Remove test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const bot = require('./index');
-const twitchBot = new bot({username:'anxietypb',oauth:'oauth:iqv7f02h5abzf7t8uhrctvyk72zk44',channels:['back2warcraft']});
-twitchBot.on('message',(m)=>console.log(m))
-twitchBot.on('message',(m)=>console.log(twitchBot.channels))
